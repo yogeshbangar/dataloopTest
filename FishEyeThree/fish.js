d7a6ef9aa1421c1f5e57afe60b098f733dfeb829
yogesh0325 <yogesh.b@dataloop.ai>
2025-05-29 14:49:41 +0400

    update code

--- FishEyeThree/fish.js
+++ FishEyeThree/fish.js
@@ -1,65 +1,56 @@
-const width = 1500; //window.innerWidth;
-const height = 1200; //window.innerHeight;
+const width = 2848; //window.innerWidth;
+const height = 2848; //window.innerHeight;
 const aspect = width / height;
 
-function configureCamera(
-    camera,
-    intrinsicData,
+function configureCamera(camera, intrinsicData, width, height) {
+  const fov = 2 * Math.atan((0.5 * height) / intrinsicData.fy);
+  camera.aspect = width / height;
+  camera.fov = (fov * 180) / Math.PI;
+  camera.setViewOffset(
+    width,
+    height,
+    0.5 * width - intrinsicData.cx,
+    0.5 * height - intrinsicData.cy,
     width,
     height
-) {
-    const fov = 2 * Math.atan((0.5 * height) / intrinsicData.fy)
-    camera.aspect = width / height
-    camera.fov = (fov * 180) / Math.PI
-    camera.setViewOffset(
-        width,
-        height,
-        0.5 * width - intrinsicData.cx,
-        0.5 * height - intrinsicData.cy,
-        width,
-        height
-    )
+  );
 
-    camera.updateProjectionMatrix()
-    camera.scale.x = intrinsicData.fy / intrinsicData.fx
+  camera.updateProjectionMatrix();
+  camera.scale.x = intrinsicData.fy / intrinsicData.fx;
 }
 
-function orientCamera(
-    camera,
-    index,
-    frame
-) {
-    const extrinsic = frame?.images?.[index]?.sensorsData?.extrinsic
-    console.log('orientCamera', frame?.images?.[index]);
-    if (
-        camera &&
-        extrinsic?.rotation &&
-        frame?.rotation &&
-        extrinsic?.translation
-    ) {
-        console.log('extrinsic~~~',extrinsic);
-        const imageQ = new THREE.Quaternion(
-            extrinsic.rotation.x,
-            extrinsic.rotation.y,
-            extrinsic.rotation.z,
-            extrinsic.rotation.w
-        ).normalize()
-        const frameQ = new THREE.Quaternion(
-            frame.rotation.x,
-            frame.rotation.y,
-            frame.rotation.z,
-            frame.rotation.w
-        ).normalize()
-        const mulQ = frameQ.multiply(imageQ)
-        camera.quaternion.copy(mulQ.multiply(new THREE.Quaternion(1, 0, 0, 0)))
-        camera.position
-            .set(
-                extrinsic.translation.x - frame.translation.x,
-                extrinsic.translation.y - frame.translation.y,
-                extrinsic.translation.z - frame.translation.z
-            )
-            .applyQuaternion(frame.rotation)
-    }
+function orientCamera(camera, index, frame) {
+  const extrinsic = frame?.images?.[index]?.sensorsData?.extrinsic;
+  console.log("orientCamera", frame);
+  if (
+    camera &&
+    extrinsic?.rotation &&
+    frame?.rotation &&
+    extrinsic?.translation
+  ) {
+    console.log("extrinsic~~~", extrinsic);
+    const imageQ = new THREE.Quaternion(
+      extrinsic.rotation.x,
+      extrinsic.rotation.y,
+      extrinsic.rotation.z,
+      extrinsic.rotation.w
+    ).normalize();
+    const frameQ = new THREE.Quaternion(
+      frame.rotation.x,
+      frame.rotation.y,
+      frame.rotation.z,
+      frame.rotation.w
+    ).normalize();
+    const mulQ = frameQ.multiply(imageQ);
+    camera.quaternion.copy(mulQ.multiply(new THREE.Quaternion(1, 0, 0, 0)));
+    camera.position
+      .set(
+        extrinsic.translation.x - frame.translation.x,
+        extrinsic.translation.y - frame.translation.y,
+        extrinsic.translation.z - frame.translation.z
+      )
+      .applyQuaternion(frame.rotation);
+  }
 }
 
 const coordinates = {
@@ -101,13 +92,14 @@ class FishEye {
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     this.renderer.setSize(width, height);
     document.body.appendChild(this.renderer.domElement);
-    this.camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-    this.composer = new THREE.EffectComposer(this.renderer);
-    this.composer.addPass(new THREE.RenderPass(this.scene, this.camera));
-    this.distortionPass = new THREE.ShaderPass(
-      DistortionShader({ k1: 0, k2: 0, k3: 0, p1: 0, p2: 0 })
-    );
-    this.composer.addPass(this.distortionPass);
+    this.camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1000);
+    // this.composer = new THREE.EffectComposer(this.renderer);
+    // this.composer.addPass(new THREE.RenderPass(this.scene, this.camera));
+    // const controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+    // this.distortionPass = new THREE.ShaderPass(
+    //   DistortionShader({ k1: 0, k2: 0, k3: 0, p1: 0, p2: 0 })
+    // );
+    // this.composer.addPass(this.distortionPass);
 
     this.setCameraProjection();
     this.cube = new THREE.Mesh(
@@ -154,19 +146,18 @@ class FishEye {
       "\nDistortion:",
       distortion
     );
-    configureCamera(
-      this.camera,
-      intrinsicData,
-      width,
-      height
+    configureCamera(this.camera, intrinsicData, width, height);
+    orientCamera(this.camera, this.cameraNo, this.data.frames[this.frameNo]);
+    // this.adjustProjectionMatrix(intrinsicData);
+    // this.distortionPass.uniforms.k1.value = distortion.k1 || 0;
+    // this.distortionPass.uniforms.k2.value = distortion.k2 || 0;
+    // this.distortionPass.uniforms.k3.value = distortion.k3 || 0;
+    // this.distortionPass.uniforms.p1.value = distortion.p1 || 0;
+    // this.distortionPass.uniforms.p2.value = distortion.p2 || 0;
+    console.log(
+      "Camera Projection Matrix:",
+      this.camera
     );
-    orientCamera(this.camera,this.cameraNo, this.data.frames[this.frameNo]);
-    this.adjustProjectionMatrix(intrinsicData);
-    this.distortionPass.uniforms.k1.value = distortion.k1 || 0;
-    this.distortionPass.uniforms.k2.value = distortion.k2 || 0;
-    this.distortionPass.uniforms.k3.value = distortion.k3 || 0;
-    this.distortionPass.uniforms.p1.value = distortion.p1 || 0;
-    this.distortionPass.uniforms.p2.value = distortion.p2 || 0;
   }
   adjustProjectionMatrix({ cx, cy, skew, fx }) {
     console.log(cx, cy, skew, fx);
@@ -182,7 +173,8 @@ class FishEye {
     requestAnimationFrame(this.animate.bind(this));
     // this.cube.rotation.y += 0.01;
     // this.cube.rotation.z += 0.01;
-    this.composer.render();
+    this.renderer.render(this.scene, this.camera);
+    // this.composer.render();
   }
   keyHandler(event) {
     const cube = this.cube;
@@ -213,6 +205,12 @@ class FishEye {
         case "1":
           this.cameraNo++;
           this.setCameraProjection();
+          break;
+        case "2":
+          if (this.cameraNo > 0) {
+            this.cameraNo--;
+            this.setCameraProjection();
+          }
           break;
       }
     }
